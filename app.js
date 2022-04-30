0f42e0700ebfd3e67b92e4300da51d48bae77667
MCprotein <chss3339@gmail.com>
2022-04-30 23:12:02 +0900

    app.listen 모듈화

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 // html로 작성하여 화면에 띄우려면 라우터나 컨트롤러 쪽에서 html을 읽은 후에 바디에 실어서 클라이언트에게 넘겨줘야 하는 번거로운 작업이 필요하지만, ejs로 하면 바로 렌더링 할 수 있다.
 // https://jogakjungbo.tistory.com/27 폴더컴팩트
 
-const PORT = 3000;
 // 라우팅
 const home = require('./routes/home');
 
@@ -17,7 +16,5 @@ app.set('view engine', 'ejs');
 
 app.use("/", home); // user -> 미들웨어를 등록해주는 메서드
 
-app.listen(PORT, () => {
-    console.log("서버 기동");
-});
+module.exports = app;
 
