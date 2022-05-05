0334f16f436c6d3a32570a9f1511cb965d8638eb
MCprotein <chss3339@gmail.com>
2022-05-06 01:47:16 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,29 @@ const output = {
     },
 };
 
+const users = {
+    id: ["woorimIT", "나개발", "김팀장"],
+    pwd: ["1234", "1234", "!23456"],
+}
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            pwd = req.body.pwd;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pwd[idx] === pwd) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인을 실패하셨습니다.",
+        })
     }
 }
 
