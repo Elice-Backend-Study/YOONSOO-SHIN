51c07b034f15fe3cb6c126b22047efb4a82d217f
MCprotein <chss3339@gmail.com>
2022-05-06 02:36:31 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const UserStorage = require("../../models/UserStorage");
+
 const output = {
     home: (req, res) => {
         res.render('home/index');
@@ -9,30 +11,23 @@ const output = {
     },
 };
 
-const users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    pwd: ["1234", "1234", "!23456"],
-}
-
 const process = {
     login: (req, res) => {
         const id = req.body.id,
             pwd = req.body.pwd;
-        
+        const users = UserStorage.getUsers("id", "pwd");
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.pwd[idx] === pwd) {
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
-
-        return res.json({
-            success: false,
-            msg: "로그인을 실패하셨습니다.",
-        })
-    }
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
+    },
 }
 
 // {key : key} key만 넣으면 value가 key와 같게 자동으로 설정됨
