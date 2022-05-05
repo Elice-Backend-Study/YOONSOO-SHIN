21e577bcfe755acf5def59506884b9e97ea3044b
MCprotein <chss3339@gmail.com>
2022-05-05 23:15:03 +0900

    로그인 API 만들기 in 서버

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 // html로 작성하여 화면에 띄우려면 라우터나 컨트롤러 쪽에서 html을 읽은 후에 바디에 실어서 클라이언트에게 넘겨줘야 하는 번거로운 작업이 필요하지만, ejs로 하면 바로 렌더링 할 수 있다.
 // https://jogakjungbo.tistory.com/27 폴더컴팩트
@@ -12,9 +13,11 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', './src/views'); // views 경로지정
 app.set('view engine', 'ejs');
-// eslint-disable-next-line no-undef
-app.use(express.static(`${__dirname}/src/public`))
 // __dirname: app.js가 있는 위치
+app.use(express.static(`${__dirname}/src/public`))
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/", home); // user -> 미들웨어를 등록해주는 메서드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,22 @@
 "use strict"
 
-const home = (req, res) => {
-    res.render('home/index');
+const output = {
+    home: (req, res) => {
+        res.render('home/index');
+    },
+    login: (req, res) => {
+        res.render('home/login')
+    },
+};
 
+const process = {
+    login: (req, res) => {
+        console.log(req.body);
+    }
 }
 
-const login = (req, res) => {
-    res.render('home/login')
-}
 // {key : key} key만 넣으면 value가 key와 같게 자동으로 설정됨
 module.exports = {
-    home,
-    login,
+    output,
+    process
 }
