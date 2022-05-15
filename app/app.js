273c871bfd71a85c0fcd8b75dab7837492791ec3
MCprotein <chss3339@gmail.com>
2022-05-15 21:48:56 +0900

    winston log관리 1

--- app/app.js
+++ app/app.js
@@ -4,13 +4,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+
 const app = express();
+dotenv.config();
 // html로 작성하여 화면에 띄우려면 라우터나 컨트롤러 쪽에서 html을 읽은 후에 바디에 실어서 클라이언트에게 넘겨줘야 하는 번거로운 작업이 필요하지만, ejs로 하면 바로 렌더링 할 수 있다.
 // https://jogakjungbo.tistory.com/27 폴더컴팩트
 
 // 라우팅
 const home = require('./src/routes/home');
+
+const logger = require("./src/config/logger");
+logger.error("HEllo");
+
 
 // 앱 세팅
 app.set('views', './src/views'); // views 경로지정
@@ -21,7 +26,13 @@ app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드 1
 
 module.exports = app;
 
+// morgan 설정
+// const morgan = require('morgan');
+// const accessLogStream = require('./src/config/log');
+// app.use(morgan("dev"));
+// app.use(morgan('common', {stream: accessLogStream}));
